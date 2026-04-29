2fa2928141aa71da74f6f5fdeb79beaccc183498
arjunharshana <arjunharshana11@gmail.com>
2026-04-29 19:49:12 +0530

    Add file for structured json response handling

---
+++ utils/response.js
@@ -0,0 +1,25 @@
+const sendSuccess = (
+  res,
+  statusCode = 200,
+  message = "Success",
+  data = null,
+) => {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    data,
+  });
+};
+
+const sendError = (res, statusCode = 500, message = "Error", errors = null) => {
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errors,
+  });
+};
+
+module.exports = {
+  sendSuccess,
+  sendError,
+};
