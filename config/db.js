8a7f076ab2e119f635f3efdf75547e2dbc509f0a
arjunharshana <arjunharshana11@gmail.com>
2026-04-29 20:03:07 +0530

    Add expression to show if db is connected successfully

--- config/db.js
+++ config/db.js
@@ -3,21 +3,31 @@ require('dotenv').config();
 
 const dbConfig = process.env.DATABASE_URL
   ? {
-      uri: process.env.DATABASE_URL,
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0,
-    }
+    uri: process.env.DATABASE_URL,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  }
   : {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0,
-    };
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  };
 
 const pool = mysql.createPool(dbConfig);
+
+// Test the database connection
+pool.getConnection()
+  .then(connection => {
+    console.log('Successfully connected to the database!');
+    connection.release();
+  })
+  .catch(err => {
+    console.error('Error connecting to the database:', err.message);
+  });
 
 module.exports = pool;
